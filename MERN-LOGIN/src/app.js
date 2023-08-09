30d0d24e7da1601e3a6ab2b035416d1b87221c1c
Alejandro Collazos Ruiz <133366937+AlejandroCollazos@users.noreply.github.com>
2023-08-09 11:35:44 -0500

    Add files via upload

--- MERN-LOGIN/src/app.js
+++ MERN-LOGIN/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './rotes/auth.routes.js';
+import taskRouter from './rotes/task.routes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -10,4 +11,5 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser()); //manjear y acceder de forma mas facil a las cookis 
 app.use("/api",authRoutes);
+app.use("/api",taskRouter);
 export default app;
---
+++ MERN-LOGIN/src/middlewares/validator.middlewares.js
@@ -0,0 +1,7 @@
+
+export const  validateSchema =(schema)=>(req, res)
+try {
+    
+} catch (error) {
+    
+}
